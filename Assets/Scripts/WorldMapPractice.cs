b4e54799d263f7c6919bc2ccdbb5c177368d96fa
GreyColor10 <halfde91@gmail.com>
2025-04-03 13:47:39 +0900

    0403 월드맵 씬 이동

---
+++ Assets/Scripts/WorldMapPractice.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+public class Practice : MonoBehaviour
+{
+    [SerializeField] Transform[] stageList;
+    [SerializeField] LayerMask stageMask;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < stageList.Length; i++)
+        {
+            stageList[i].name = $"{i + 2}";
+        }
+        DataManager.instance.SaveData(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.allCameras[0].ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, stageMask))
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                int connectSceanNum = Convert.ToInt32(hit.transform.name);
+                if (connectSceanNum <= DataManager.instance.nowMap.clear)
+                {
+                    //Popup UI 등 필요한 기능
+                    LoadSystem.LoadScene(connectSceanNum);
+                }
+            }
+        }
+        //레이캐스트로 레이어를 확인하고 오브젝트 이름과 이어지는 스테이지의 씬넘버를 같게 함
+    }
+}
