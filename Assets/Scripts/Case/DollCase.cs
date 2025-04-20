cf88ce5afad3d86d9c0551fc80dc74a6b7cde34e
GreyColor10 <halfde91@gmail.com>
2025-04-21 07:49:24 +0900

    시작 씬부터 스테이지 3 클리어까지 작업 완료

--- Assets/Scripts/Case/DollCase.cs
+++ Assets/Scripts/Case/DollCase.cs
@@ -4,11 +4,10 @@ using static DollInteraction;
 public class DollCase : AnimProperty, IDollInteraction
 {
     public GameObject Doll;
-    public Transform dollCase;
 
     public void Interact()
     {
-        if (!Physics.BoxCast(dollCase.position + new Vector3(0, 1, 0.5f),new Vector3(0.4f,0.9f,0.4f),Vector3.forward, Quaternion.identity,1f))
+        if (!Physics.BoxCast(transform.position + new Vector3(0, -0.5f, 0.5f),new Vector3(0.4f,0.9f,0.4f),Vector3.forward, Quaternion.identity,1f))
         {
             Doll.transform.SetParent(null);
             myAnim.SetTrigger("DollCaseOpen");
